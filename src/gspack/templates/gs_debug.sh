570fae2792eb5f3670a813019dce346de8f7bf26
jakobkotas <jakobkotas@gmail.com>
2025-01-16 13:16:25 -0800

    Update gs_debug.sh

--- src/gspack/templates/gs_debug.sh
+++ src/gspack/templates/gs_debug.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 python3.7 -m pip uninstall -y gspack
-git clone https://github.com/aksholokhov/gspack
+git clone https://github.com/jakobkotas/gspack
 cd gspack || exit
 python3.7 setup.py develop
